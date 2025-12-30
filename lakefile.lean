ff9b150842feffa21617a5310a153c93c2b79c1f
sosudo <shivansh.gour90@gmail.com>
2025-12-30 01:07:48 -0500

    initial commit

---
+++ lakefile.lean
@@ -0,0 +1,16 @@
+import Lake
+open Lake DSL
+
+package eval_InformationTheory where
+  srcDir := "src"
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.17.0"
+
+require VerifiedAgora from git
+  "https://github.com/stagiralabs/VerifiedAgora.git" @ "v4.17.0"
+
+@[default_target]
+lean_lib «Mathlib.InformationTheory» where
+  roots := #[`Mathlib.InformationTheory]
+  globs := #[.submodules `Mathlib.InformationTheory]
